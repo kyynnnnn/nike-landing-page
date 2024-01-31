6b2707771edb23ae5286bd1aa798ad2ce36ca929
rdtyaa <maoreendsafiraa@gmail.com>
2024-02-01 00:01:13 +0700

    first

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import CustomerReviews from "./CustomerReviews";
+import Footer from "./Footer";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import Subcribe from "./Subcribe";
+import SuperQuality from "./SuperQuality";
+
+export {
+  Hero,
+  PopularProducts,
+  CustomerReviews,
+  Footer,
+  Services,
+  SpecialOffer,
+  Subcribe,
+  SuperQuality,
+};
